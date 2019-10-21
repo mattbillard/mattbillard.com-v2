40eae182d7dae83115f627ecce646de0a67e0117
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2019-10-21 14:39:09 -0400

    Router

--- react/src/components/app/app.tsx
+++ react/src/components/app/app.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom'
 import { About, Contact, Home, Skills } from '../';
 import './app.css';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
-      App
-      <Home />
-      <About />
-      <Skills />
-      <Contact />
+      <BrowserRouter>
+        <h1>App</h1>
+        <Link to='/home'>home </Link> 
+        <Link to='/about'>about </Link> 
+        <Link to='/skills'>skills </Link> 
+        <Link to='/contact'>contact </Link> 
+
+        <Switch>
+          <Route path='/home' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/skills' component={Skills}/>
+          <Route path='/contact' component={Contact}/>
+          <Redirect from="/*" to="/home" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
